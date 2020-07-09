74a7b62894533e32f247c2e46091473197ea7d79
andreacapntd <andreacapntd@gmail.com>
2020-07-09 16:17:05 +0200

    ex js simon funzionante

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// Un alert espone 5 numeri casuali (univoci).
-// Poi parte un timer di 30 secondi.
-// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
 
+
+// Un alert espone 5 numeri casuali (univoci).
 var numeriCpu = [];
 for (var i = 0; i < 5; i++) {
   var numeriRandom = getRandomIntInclusive(1, 100);
@@ -10,6 +8,24 @@ for (var i = 0; i < 5; i++) {
 }
 alert('I numeri da ricordare sono' + ' ' + numeriCpu);
 
+// Poi parte un timer di 30 secondi.
+setTimeout(function() {
+  // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+  var numeriUtente = [];
+  var risposteUtente;
+  for (var i = 0; i < 5; i++) {
+    risposteUtente = parseInt(prompt('Inserisci il numero'))
+    var controllo = inArray (numeriCpu, risposteUtente);
+    if (controllo) {
+      numeriUtente.push(risposteUtente);
+    }
+
+  }
+  // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+  alert('Hai indovinato' + ' ' + numeriUtente.length + ' ' + ' numeri')
+  alert('I numeri indovinati sono' + ' ' + numeriUtente)
+
+} , 30000);
 
 
 
