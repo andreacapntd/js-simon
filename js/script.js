7af293d8593115320352fdbcdcc8ed6f8f89f782
andreacapntd <andreacapntd@gmail.com>
2020-07-10 15:10:31 +0200

    ex js simon correzione

--- js/script.js
+++ js/script.js
@@ -1,39 +1,16 @@
-
-
 // Un alert espone 5 numeri casuali (univoci).
 var numeriCpu = [];
-for (var i = 0; i < 5; i++) {
+while (numeriCpu.length < 5) {
   var numeriRandom = getRandomIntInclusive(1, 100);
-  numeriCpu.push(numeriRandom)
+  if (numeriCpu.includes(numeriRandom) == false) {
+    numeriCpu.push(numeriRandom)
+  }
 }
+console.log(numeriCpu);
 alert('I numeri da ricordare sono' + ' ' + numeriCpu);
 
 // Poi parte un timer di 30 secondi.
-setTimeout(function() {
-  // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-  var numeriUtente = [];
-  for (var i = 0; i < 5; i++) {
-    var risposteUtente = parseInt(prompt('Inserisci il numero'));
-    var controllo = inArray (numeriCpu, risposteUtente);
-    if (controllo) {
-      numeriUtente.push(risposteUtente);
-    }
-  }
-
-  // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
-  alert('Hai indovinato' + ' ' + numeriUtente.length + ' ' + ' numeri')
-  alert('I numeri indovinati sono' + ' ' + numeriUtente)
-
-} , 30000);
-
-
-
-
-
-
-
-
-
+setTimeout(askNumberUser,2000);
 
 
 //Funzioni
@@ -64,4 +41,29 @@ function checkRange(min,max,num) {
     return true;
   }
   return false;
+}
+
+//funzione di gioco utente
+function askNumberUser() {
+  var numeriUtente = [];
+
+  while (numeriUtente.length < numeriCpu.length) {
+   var risposteUtente = parseInt(prompt('Inserisci il numero'));
+   if (!numeriUtente.includes(risposteUtente) && !isNaN(risposteUtente)) {
+    numeriUtente.push(risposteUtente);
+   }
+  }
+  var numeriUtenteOk = [];
+
+
+  for (var i = 0; i < numeriUtente.length; i++) {
+    if (numeriCpu.includes(numeriUtente[i])) {
+      numeriUtenteOk.push(numeriUtente[i]);
+    }
+  }
+
+  alert('Hai indovinato' + ' ' + numeriUtenteOk.length + ' ' + ' numeri');
+  alert('I numeri indovinati sono' + ' ' + numeriUtenteOk);
+
+
 }
